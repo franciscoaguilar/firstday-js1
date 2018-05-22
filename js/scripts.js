42e260c67064b752fc2bffe35be2372917665798
francisco <0667034@my.scccd.edu>
2018-05-22 16:50:34 -0700

    day1 homework complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
-var myName = "Ryeker";
+var myName = "Francisco";
+var myExpectations = "Learn how to enhance websites using Javascript"
+var myHobbie = "My favorite hobbie is vinyl wrapping cars"
 console.log(myName);
+console.log(myExpectations);
+console.log(myHobbie); 
