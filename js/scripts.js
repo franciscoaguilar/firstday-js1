11170c37e504dc13a438c24643b7bb3cb1ce859a
francisco <0667034@my.scccd.edu>
2018-05-23 19:50:14 -0700

    name , age goal

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 var myName = "Francisco";
 var myExpectations = "Learn how to enhance websites using Javascript"
 var myHobbie = "My favorite hobbie is vinyl wrapping cars"
+
 console.log(myName);
 console.log(myExpectations);
-console.log(myHobbie); 
+console.log(myHobbie);
+
+///DAY 2
+
+//
+///
+////
+ let dozen =12; {
+   let dozen =13;
+   console.log(dozen)
+ }
+ console.log(dozen);
+///
+var myName="Francisco";
+let age = 23;
+const goal= "learn java";
+
+console.log(myName, age, goal);
