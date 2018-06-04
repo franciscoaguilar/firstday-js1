1dd34d0094c6e01016c86be7d2b786cf04ba9bb6
francisco <0667034@my.scccd.edu>
2018-06-04 13:19:40 -0700

    day 4 completed hw

--- js/scripts.js
+++ js/scripts.js
@@ -1,129 +1,4 @@
 /*
-var myName = "Francisco";
-var myExpectations = "Learn how to enhance websites using Javascript"
-var myHobbie = "My favorite hobbie is vinyl wrapping cars"
-
-console.log(myName);
-console.log(myExpectations);
-console.log(myHobbie);
-
-///DAY 2
-
-//
-///
-////
- let dozen =12; {
-   let dozen =13;
-   console.log(dozen)
- }
- console.log(dozen);
-///
-var myName="Francisco";
-let birthday = new Date('05/09/1995');
-
-
-/*
-const obj= new String ( "learn java");
-console.log(obj);
-alert(user);
-let userAnswer= confirm("did you brush teeth");
-console.log(userAnswer);
-*/
-/*
-let favriotMovie = "Fast and furious ";
-let favriotfood = "pupusas ";
-const year = new Date('march 24, 2008');
-const favFoodingrediants = ['cheese', 'flour'];
-
-alert(favriotfood +  favFoodingrediants);
-confirm (favriotMovie + year);
-
-//homework day 2
- let userName = prompt("what is your name ");
- let userLastName = prompt("what is your last name ");
- let userBirthday = prompt( "whats your b-day ");
- let userb = new Date(userBirthday);
-
- console.log(userName);
- console.log(userLastName);
- console.log(userBirthday);
-
- alert(userName);
- alert(userLastName);
- confirm(userb);
- */
-
-
-
-
-// Day 3 homework
-const name = ""; //falsy
-const age = 23; //truey
-if(name && age ){
-  alert('one exist');
-}
-const numAge = 32;
-const stringAge = '32';
-//if(!numAge == stringAge) {
-  //alert('they\'re the same!');
-// }
-let objectOne = {
-  name: "Francisco"
-};
-let objectTwo = {
-  name: "Francisco"
-};
-//alert(objectOne == objectTwo); //always false, two objects can never be equal too eachother
-if (numAge <= stringAge.length){
-  alert("what?");
-}
-var myString = "apple";
-var objString = new String ("apple");
-//alert ( myString === objString)// displays false
-//alert ( myString == objString) // display true
-const limit =12;
-// console.log(limit);//result 12
-// limit++; //shorthand for limit =limit+1; result 13
-// limit =limit+1; // result 13
-// limit += 7; //result: 19 adds 12+7
-// limit += 5; // result: 17 adds 12 +5
-
-// let myNumber = "a";
-// console.log(myNumber + "b");// displays ab
-
-// let myNumber = 1;
-// console.log(myNumber + 2);// displays 3
-/*
-let myNumber = "cheese";
-isNaN(myNumber) = 77;
-console.log(!isNaN(myNumber));
-*/
-/*
-let firstNumber = parseInt( prompt("give number", "10"));
-let secondNumber = parseInt( prompt("give number", "10"));
-alert(firstNumber + secondNumber);
-
-if (firstNumber<secondNumber){
-  alert("yay");
-}
-else alert("boo");
-*/
-/*
-let firstName = prompt('what is your first name ', 'tom');
-switch (firstName.toLowerCase()) {
-  case 'tom':
-    alert('hello Tom!');
-    break;
-  case 'john':
-  case 'henry':
-  case 'peter':
-    alert('common name');
-    break;
-  default:
-  alert('never heard of it ');
-  break;
-}
-*/
 // HW DAY 3
 let firstName = prompt('what is your first name ', 'tom');
 let userColor = prompt("whats your fav color: red, orange, yellow, green, blue, pink, purple, black, white, and grey ");
@@ -162,3 +37,38 @@ switch (userColor.toLowerCase()){
   alert(firstName + " hey that was not a color ");
   break;
 }
+*/
+ /* var favoriteMovie = ['Star Wars VI', '1983'];
+   var secondFavorite = ['Return of the King', '2003'];
+   var thirdFavorite = ['Equilibrium', '2002'];
+   var myFavoriteMovies = [];
+   var movieYears = [];
+
+    myFavoriteMovies.push(favoriteMovie, secondFavorite, thirdFavorite);
+ movieYears.push(myFavoriteMovies[0][0], myFavoriteMovies[1][0],myFavoriteMovies[2][0]);
+ alert(myFavoriteMovies);
+ alert(movieYears);
+ */
+
+
+var topMovie1 =['1: coco ', 'Miguel '];
+var topMovie2 =['2: instructions not included ', 'Valentin '];
+var topMovie3 = [ '3: spiderman ', 'peter '];
+var topMovie4 = ['4: avengers ', 'too many to name '];
+var topMovie5 = ['5: fast and furious ', 'bryan '];
+var topMovies = [];
+var topMoviestitles = [];
+topMovies.push(topMovie1, topMovie2, topMovie3, topMovie4, topMovie5);
+topMoviestitles.push(topMovies[0][0], topMovies[1][0], topMovies[2][0], topMovies[3][0], topMovies[4][0], );
+alert(topMoviestitles);
+var favAndleastFav =[];
+favAndleastFav.push(topMovie1[0], topMovie5[0]);
+console.log("favrot and least " + favAndleastFav);
+alert(topMovies.length);
+
+
+var i, len, text;
+for (i = 0, len = topMovies.length, text = ""; i < len; i++) {
+    var found= Array.prototype.find(topMovie1);
+}
+console.log(found);
