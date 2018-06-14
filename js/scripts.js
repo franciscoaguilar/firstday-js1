cb5cd6166e061dbcf7d8a0d67b3e7a87f316b1c9
francisco <0667034@my.scccd.edu>
2018-06-13 19:03:52 -0700

    day6 finished work

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,91 @@
-// let body =
-// document.getElementsByTagName('body');
-// // array
-// // console.log(body[0]);
-// let classes =
-// document.getElementsByClassName('my-header');
-// // console.log(classes[0]);
-// let id =
-// document.getElementById('my-paragragh');
-// console.log(id)
+// let body = document.getElementsByTagName('body'); //array
 //
-// // Newer methods:
-// let tags = document.querySelector('body');
-// console.log(tag);
+// let classes = document.getElementsByClassName("my-header");
 //
-// let myclass =
-// document.querySelector('.my-header');
-// console.log(myclass);
+// let id = document.getElementById("my-paragraph");
 //
-// let myId = document.querySelector('#my-paragragh');
+// //Newer methods:
+// let tag = document.querySelector('body');
+//
+// let myClass = document.querySelector('.my-header');
+//
+// let myId = document.querySelector('#my-paragraph');
 // console.log(myId);
 //
-// // return array with all that you are looking for
-// let tags =
-// document.querySelectorAll('body');
+// let tags = document.querySelectorAll('body');
+//
+// //TRY IT OUT
+// function queryDom(methodName, element) {
+//   switch(methodName.toLowerCase()) {
+//     case 'getelementsbytagname':
+//       console.log(document.getElementsByTagName(element));
+//       break;
+//     case 'getelementsbyclassname':
+//       console.log(document.getElementsByClassName(element));
+//       break;
+//     case 'getelementbyid':
+//       console.log(document.getElementById(element));
+//       break;
+//     case 'queryselector':
+//       switch(requestQuerySelectorType()) {
+//         case 'class':
+//           console.log(document.querySelector(`.${element}`));
+//           break;
+//         case 'id':
+//           console.log(document.querySelector(`#${element}`));
+//           break;
+//         default:
+//           console.log(document.querySelector(element));
+//           break;
+//       }
+//       break;
+//       case 'queryselectorall':
+//       switch(requestQuerySelectorType()) {
+//         case 'class':
+//           console.log(document.querySelectorAll(`.${element}`));
+//           break;
+//         case 'id':
+//           console.log(document.querySelectorAll(`#${element}`));
+//           break;
+//         default:
+//           console.log(document.querySelectorAll(element));
+//           break;
+//       }
+//       break;
+//     default:
+//       break;
+//   }
+// };
+//
+// function requestQuerySelectorType() {
+//   return prompt("Which of the three query types do you want me to use?", "please answer 'class', 'id', or 'tag'");
+// };
+//
+// queryDom(prompt('what method do you want to use?'), prompt('what do you want to query for?'));
+// let myClass = document.querySelector('.my-header');
+//
+// myClass.addEventListener(
+//   'click', //first arg
+//   e =>{
+//     alert('you clicked on me!!');
+//   }
+// )
+
+let button =document.querySelector('button');
+let div = document.querySelector('div');
+let h1 =document.querySelector('h1');
 
 
-function queryDom(methodName, element){
-  switch(methodName.toLowerCase()){
-    case 'getelementbytagname':
-      documenet.querySelector(element);
-      break;
-      case 'getelementclassname':
-        documenet.querySelector(element);
-        break;
-      // add 4 more
-      case 'queryselector' :
-      document.querrySelector(`.${element}`);
-      break;
-    default;
-    break;
-  }
+button.addEventListener('click', e =>{
+e.target.textContent ="goodbye cats"
+})
 
-}
-myClass.addEventListener('click')
+div.addEventListener('mouseenter', e =>{
+e.target.style.backgroundColor = "#e40007";
+console.log(e);
+})
+
+h1.addEventListener('mouseleave', e =>{
+e.target.style.color = "#00ffff"
+console.log(e);
+})
