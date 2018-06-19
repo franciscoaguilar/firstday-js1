66e9b8c3f508da20d74b2ed87df386b05d31db13
francisco <0667034@my.scccd.edu>
2018-06-18 20:55:18 -0700

    day8 end of class notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,54 @@
+//
+// let passwordInput =  document.forms.login.password;
+// let emailInput = document.forms.login.email;
+// let myPTag = document.querySelector('#data_binding');
+// let submit = document.forms.login.Submitbtn;
+//
+// emailInput.addEventListener('keyup', e => {
+//   myPTag.textContent = e.target.value;
+// });
+//
+// submit.addEventListener('click', e => {
+//     if (passwordInput.value === ''|| emailInput.value ===''){
+//       e.preventDefault();
+//     }
+// })
 
-// SQUARES A NUMBER
-function squareNumber(number){
-  if ( !isNaN(number)){
-  return number * number;
- }
-   else{
-     alert("NaN");
-      usersNumber = prompt('give another answer', '6');
-      squareNumber(parseInt(usersNumber));
-  }
+// call how it works with this
+let user = {
+  name: 'henry',
+  password: 12345,
+  status: 'active'
 };
-let usersNumber = prompt('give a number to square', '6');
-let answer = squareNumber(usersNumber);
-alert(answer);
-
-// CAPITILIZE THE FIRST LETTER OF A STRING AND ADDS A PERIOD IF IT DOESNT ALREADY HAVE IT
-function capFirstletter(word){
-  if (usersWord.endsWith('.')){
-    return word[0].toUpperCase() + word.substring(1);
-  }
-  else {
-    return   word[0].toUpperCase() + word.substring(1) + '.';
-  }
-
+let guest = {
+  name: "guest",
+  password: undefined,
+  status: "inactive"
 };
-let usersWord = prompt('give a word and ill capitilize the first letter', 'apple.');
-let upperWord = capFirstletter( usersWord);
-alert(upperWord);
-
-// SPLITS WORD IN HALF AND REVERSES IT
-function flipsWord (flip){
-  return flip.substring(flip.length/2) + flip.charAt(0) + flip.charAt(1);
+let newScope = {
+  name: "jose",
+  password:" pass",
+  instrument: 'pipees',
+  context: logMyName
 }
-
-
-let askFlippedWord = prompt('ADDS FIRST HALF OF THE WORD TO THE BACK', 'word');
-let resultsFlippedWord = flipsWord(askFlippedWord);
-alert (resultsFlippedWord);
-
-// CHECKS IF THE WWORD IS PALINDROME
-function checkIfpalindrome(palin){
-   let reverse = palin.split("").reverse().join("");
-   return reverse == palin;
- }
-let askPalin = prompt('give a word to check if it is palindrome', 'civic');
-let resultsFlipped = checkIfpalindrome(askPalin);
-alert(resultsFlipped);
-
-
-
+function logMyName(greeting){
+  console.log(this.name, this.password);  //defenitin
+  alert(`${greeting}, ${this.name}!`);
+}
+// newScope.context("fly away"); //impicit binding
+// logMyName.call(user, "helo"); //call site
+// logMyName.call(guest, "sign up");
+// logMyName.apply(user, ["helo"]); //used for arrays
+//unless you are using a fat arrow function this is in the global context
+// variables are not bound to this
+// if you wanna change this refrence you can use call or apply
+let classScore = 90;
+const scopeOne ={
+  classScore: 75,
+  scopeTwo: {
+    classScore: 50
+  }
+};
+console.log(classScore);
+console.log(scopeOne.classScore);
+console.log(scopeOne.scopeTwo.classScore);
