12b7ab11b3563745c9ece3ebb19dbe71335c832f
francisco <0667034@my.scccd.edu>
2018-06-18 18:03:00 -0700

    day7 wor

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,53 @@
+let button =
+document.querySelector('button');
+let div = document.querySelector('div');
+const body = document.querySelector('body');
+button.addEventListener('click', e => {
+  e.target.textContent = "Goodbye cats!"
+});
 
-// SQUARES A NUMBER
-function squareNumber(number){
-  if ( !isNaN(number)){
-  return number * number;
- }
-   else{
-     alert("NaN");
-      usersNumber = prompt('give another answer', '6');
-      squareNumber(parseInt(usersNumber));
-  }
-};
-let usersNumber = prompt('give a number to square', '6');
-let answer = squareNumber(usersNumber);
-alert(answer);
-
-// CAPITILIZE THE FIRST LETTER OF A STRING AND ADDS A PERIOD IF IT DOESNT ALREADY HAVE IT
-function capFirstletter(word){
-  if (usersWord.endsWith('.')){
-    return word[0].toUpperCase() + word.substring(1);
-  }
-  else {
-    return   word[0].toUpperCase() + word.substring(1) + '.';
-  }
-
-};
-let usersWord = prompt('give a word and ill capitilize the first letter', 'apple.');
-let upperWord = capFirstletter( usersWord);
-alert(upperWord);
-
-// SPLITS WORD IN HALF AND REVERSES IT
-function flipsWord (flip){
-  return flip.substring(flip.length/2) + flip.charAt(0) + flip.charAt(1);
-}
+div.innerHTML = "<p>dont really want to use this, user can send you malicious software but can be used to generate things example, this is used in wordpress to generate a button when you drag an drop</p>";
 
 
-let askFlippedWord = prompt('ADDS FIRST HALF OF THE WORD TO THE BACK', 'word');
-let resultsFlippedWord = flipsWord(askFlippedWord);
-alert (resultsFlippedWord);
+//create element
+let newH1 = document.createElement('h1');
+console.log(newH1);
+newH1.textContent = "Login here!!" //.textContent adds text
+// body.appendChild(newH1)//appendChild adds to your element
+newH1.classList.add('text-center')
+;
+newH1.className = "text-center";
+body.insertBefore(newH1, div);//overall patrent you want to insert element into, calling parent (body), what you are adding/new node (newH1), refrence node (div)
 
-// CHECKS IF THE WWORD IS PALINDROME
-function checkIfpalindrome(palin){
-   let reverse = palin.split("").reverse().join("");
-   return reverse == palin;
- }
-let askPalin = prompt('give a word to check if it is palindrome', 'civic');
-let resultsFlipped = checkIfpalindrome(askPalin);
-alert(resultsFlipped);
+div.id = "red_text";
+div.classList.add('text-center');
 
-
-
+div.addEventListener('click', e =>{
+  div.classList.toggle('text-center');
+  // console.log(div.classList.contains('text-center'))
+  div.classList.replace('green-background');
+})
+// create image tag
+myImg = document.createElement('img')
+;
+// set sort setAttribute
+myImg.setAttribute('src', 'https://i.ytimg.com/vi/y8-R1H9e1j0/maxresdefault.jpg');
+// add the image
+div.appendChild(myImg);
+console.log(myImg);
+// let body = document.querySelector('body'); //refrence to body tag
+// for ( i= 0; i <= 100; i++) {
+// let p = document.createElement('p');
+//  if (  i % 3 ==0 && i % 5 ==0){
+//   p.textContent= 'fizzbuzz'
+// }
+//  else if ( i % 3==0){
+//    p.textContent = 'fizz';
+//  }
+//  else if ( i % 5==0){
+//    p.textContent = 'buzz';
+//  }
+//  else { p.textContent = i;
+//  }
+// body.appendChild(p);
+// }
