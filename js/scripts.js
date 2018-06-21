cac9f3db15eed4364f5ed86317fe57778faf41ce
francisco <0667034@my.scccd.edu>
2018-06-20 18:03:52 -0700

    day8done

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 
-let passwordInput =  document.forms.login.password;
+// let passwordInput =  document.forms.login.password;
 let emailInput = document.forms.login.email;
 let myPTag = document.querySelector('#data_binding');
 let submit = document.forms.login.Submitbtn;
+let createElement = document.createElement('li');
+let list = document.getElementById('list')
+let textnode = document.createTextNode(emailInput);
+createElement.appendChild(textnode);
+    document.getElementById("list").appendChild(createElement);
 
-emailInput.addEventListener('click', e => {
-  myPTag.textContent = e.target.value;
-  display(myPtag.textContent)
+
+
+
+
+
+emailInput.addEventListener('onclick', e => {
+  createElement.appendChild(textnode);
+      document.getElementById("list").appendChild(createElement);
 });
 
 submit.addEventListener('click', e => {
@@ -16,28 +26,28 @@ submit.addEventListener('click', e => {
 })
 //
 // call how it works with this
-let user = {
-  name: prompt('what is your name'),
-  password: 12345,
-  status: 'active'
-};
-let guest = {
-  name: "guest",
-  password: undefined,
-  status: "inactive"
-};
+// let user = {
+//   name: prompt('what is your name'),
+//   password: 12345,
+//   status: 'active'
+// };
+// let guest = {
+//   name: "guest",
+//   password: undefined,
+//   status: "inactive"
+// };
 // let newScope = {
 //   name: "jose",
 //   password:" pass",
 //   instrument: 'pipees',
 //   context: logMyName
 // }
-function logMyName(greeting){
-  console.log(this.name, this.password);  //defenitin
-  alert(`${greeting}, ${this.name}!`);
-}
-// newScope.context("fly away"); //impicit binding
-logMyName.call(user, "helo"); //call site
+// function logMyName(greeting){
+//   console.log(this.name, this.password);  //defenitin
+//   alert(`${greeting}, ${this.name}!`);
+// }
+// // newScope.context("fly away"); //impicit binding
+// logMyName.call(user, "helo"); //call site
 // logMyName.call(guest, "sign up");
 // logMyName.apply(user, ["helo"]); //used for arrays
 //unless you are using a fat arrow function this is in the global context
